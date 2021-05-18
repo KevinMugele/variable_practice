f7726cc147fa27536082bb89fba1a883bf1ff201
Kevin Mugele <kevin.mugele@gmail.com>
2021-05-17 22:16:40 -0400

    Add hash

--- model-1.rb
+++ model-1.rb
@@ -11,3 +11,11 @@ had_a_campfire = true
 
 campers = ['Kevin', 'Michael', 'Nicolas', 'Joshua']
 types_of_food = ['hot dogs', 'hamburgers', 'mac n cheese']
+
+campsite_address = {
+  street_number: '5644';
+  road: 'NY-86';
+  city: 'Wilmington';
+  state: 'New York';
+  zip_code: '12997'
+}
